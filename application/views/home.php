b08b48ac081fce3289f683266d584b2ff667a44c
Tapha Ngum <tapha@live.co.uk>
2011-04-02 17:17:20 +0300

    new commit

---
+++ application/views/home.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>"The Simplest Fitness And Nutrition Tool Online"</title>
+	<link rel="icon" type="image/png" href="<?php echo $base_url."images/favicon.ico";?>">
+	<link rel="stylesheet" type="text/css" href="<?php echo $base_url.'css/style.css';?>" />
+</head>
+<body>
+<div id='main'>
+	<div id='top'>
+		<div id='logo' style="width:230px;">
+			<img class='mini_logo' src='<?php echo $base_url.'images/food.png';?>'>
+			<a id='burnmate_name' href='<?php echo $base_url; ?>'>Burnmate</a>
+		</div>
+	<div id='menu'>
+	 <a href="/howto/">howto</a> &middot;	
+	 <a href="/signup/">signup</a> &middot;	
+	 <a href="/benefits/">benefits</a> &middot;	
+	 <a href="/stats/">stats</a> &middot;	
+	 &nbsp;&nbsp;&nbsp;&nbsp; &middot;	
+	 <a href="/">log in</a>   &middot;	
+	</div>
+	<div style="clear:both"></div>	
+	</div>
+	
+	<div id='right_main' style="width:450px;float:left">
+		<div style="margin-top:30px;margin-left:10px;">
+ <p style="font-size:2em">Welcome to Burnmate!</p>
+    <p style="color:#852">"The Simplest Fitness And Nutrition Tool Online" </p>
+	<p>No User Interface, Just Email</p>
+
+
+       <br>
+       <div style="width:250px;padding-left:10px;padding-top:10px;padding-bottom:5px;background:#fff;border:2px solid fff">
+       <p style="font-size:1.6em;font-weight:normal;text-decoration:none">
+       <span style="color:#057">&#8594;</span> <a style="text-decoration:underline;color:#057" href="/signup/">sign up now for $9.99</a></p>
+</div>
+
+<div style="width:350px;margin-right:30px;margin-top:50px;font-size:1em">
+
+	
+    <p>Burnmate is the simplest way to track your fitness and nutrition.</p>
+    <p><b> How It Works </b></p>
+    <p class='explain'>Email your progress to "ate@burnmate.com" (for food) or "burn@burnmate.com" (for workouts) in 140 chars or less. Per meal or workout.</p>
+	<p class='explain'> e.g. "Scrambled Eggs:400" if you ate 400 cals worth of scrambled eggs</p>
+	<p class='explain'>We store that and send you daily, weekly and annual progress reports based on your goals so you know how you're doing.</p>
+	<p class='explain'>Shimples!</p>
+<br>
+
+    <p>See our <a href="<?php echo $base_url.'benefits' ;?>">summary list of benefits</a> and discover why this might just be the tool for you.</p>
+<br>
+</div>
+    </div>
+	</div>
+</div>
+</body>
+</html>
